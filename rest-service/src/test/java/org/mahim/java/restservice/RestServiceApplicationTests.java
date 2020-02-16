968d54ebf0e4706536b6f584254584d5fe6ce677
mahim007 <ashrafulmahim@gmail.com>
2020-02-16 21:52:57 +0600

    simple demo rest services

---
+++ rest-service/src/test/java/org/mahim/java/restservice/RestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mahim.java.restservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
