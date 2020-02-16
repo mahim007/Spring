a03c6ca8d239856d6667df681334971646241bcc
mahim007 <ashrafulmahim@gmail.com>
2020-02-17 00:45:36 +0600

    schedule example

--- rest-service/src/main/java/org/mahim/java/restservice/RestServiceApplication.java
+++ rest-service/src/main/java/org/mahim/java/restservice/RestServiceApplication.java
@@ -2,8 +2,10 @@ package org.mahim.java.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
