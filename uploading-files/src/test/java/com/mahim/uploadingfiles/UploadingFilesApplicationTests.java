dc32b2514d1984ae80da7d4dad68c6a3ab7d9d1f
mahim007 <ashrafulmahim@gmail.com>
2020-02-27 23:02:02 +0600

    uploading files as multipart completed

---
+++ uploading-files/src/test/java/com/mahim/uploadingfiles/UploadingFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahim.uploadingfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadingFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
