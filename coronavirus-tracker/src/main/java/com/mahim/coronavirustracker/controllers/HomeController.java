f78d380ba42c0c55ecb29ddb88da2ee0b5aa02de
mahim007 <ashrafulmahim@gmail.com>
2020-03-07 23:16:16 +0600

    corona virus tracker application done

---
+++ coronavirus-tracker/src/main/java/com/mahim/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,28 @@
+package com.mahim.coronavirustracker.controllers;
+
+import com.mahim.coronavirustracker.models.LocationStats;
+import com.mahim.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int diffFromPrevDay = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("diffFromPrevDay", diffFromPrevDay);
+
+        return "home";
+    }
+}
